1be648fd9b877e55085d28f9506d0c0d5f3eb7b0
martinOlmos <martin.olmos@ase-sinaloa.gob.mx>
2026-05-22 10:39:46 -0700

    [ADD]: Metadata

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@ang
 import { Pokemon } from '../../pokemon/interfaces';
 import { PokemonService } from '../../pokemon/services/pokemon.service';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemon-page',
@@ -12,12 +14,30 @@ import { ActivatedRoute } from '@angular/router';
 export default class PokemonPageComponent implements OnInit {
   private pokemonService = inject(PokemonService);
   private route = inject(ActivatedRoute);
+  private title = inject(Title);
+  private meta = inject(Meta);
+
   public pokemon = signal<Pokemon | null>(null);
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (!id) return;
 
-    this.pokemonService.loadPokemon(id).subscribe(this.pokemon.set);
+    this.pokemonService
+      .loadPokemon(id)
+      .pipe(
+        tap(({ name, id }) => {
+          const pageTitle = `#${id} - ${name.toLocaleUpperCase()}`;
+          const pageDescription = `Página del Pokemon ${name}`;
+          const pageImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+          this.title.setTitle(pageTitle);
+
+          this.meta.updateTag({ name: 'description', content: pageDescription });
+          this.meta.updateTag({ name: 'og:title', content: pageTitle });
+          this.meta.updateTag({ name: 'og:description', content: pageDescription });
+          this.meta.updateTag({ name: 'og:image', content: pageImage });
+        }),
+      )
+      .subscribe(this.pokemon.set);
   }
 }
