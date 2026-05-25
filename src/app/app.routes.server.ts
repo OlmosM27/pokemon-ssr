9d94b635ec5da273a59cfa7e50175b0124a1e194
martinOlmos <martin.olmos@ase-sinaloa.gob.mx>
2026-05-25 13:11:36 -0700

    [FIX]: Corrección de las rutas de servidor

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,8 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemon/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const data = await res.json();
+      return data.results.map((_: unknown, i: number) => ({ id: String(i + 1) }));
+    },
+  },
+  {
     path: '**',
-
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Prerender,
   },
 ];
