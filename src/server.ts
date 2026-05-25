015c282a195073b2590306de2c627c59cd765d22
martinOlmos <martin.olmos@ase-sinaloa.gob.mx>
2026-05-25 11:15:07 -0700

    [ADD]: archivos para corrección de ssr

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
+// !IMPORTANT: Codigo para Netlify
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import { getContext } from '@netlify/angular-runtime/context.mjs';
 
-const angularAppEngine = new AngularAppEngine({
-  allowedHosts: [
-    'localhost',
-    'pokemon-ssr-mo.netlify.app', // 👈 sin https://
-  ],
-});
+const angularAppEngine = new AngularAppEngine();
 
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
+export async function netlifyAppEngineHandler(
+  request: Request
+): Promise<Response> {
   const context = getContext();
 
   const result = await angularAppEngine.handle(request, context);
   return result || new Response('Not found', { status: 404 });
 }
 
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
 export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
